b1dd59c86579af13659de01d2420132d89c55ff7
Andrew Buchan <andyhasit@gmail.com>
2014-12-10 12:31:38 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-web2py_intellisense
+Web2py Intellisense
 ===================
 
-a module to add fake intellisense to web2py objects
+Adds (fake) auto completion and hints to web2py global objects (such as db, request etc...) when working with an editor which provides Python code completion (such as [PyScripter](https://code.google.com/p/pyscripter/)).
+
+###Why?
+
+Because auto completion and hints makes writing code so much faster! 
+
+Unfortnately in web2py what we really want code completion on are the models (db.my_table.my_field etc...) but these are built up dynamically at every request (as opposed to in django where they are class definitions) so that's just not possible.
+
+Unless we cheat :)
+
+###How?
+
+
+
+By using a live instance of db object, we can write dummy class definitions mimicking the tables & fields to a file. 
+
+We can then fake import that file:
+
+        if 0:
+            import autocomplete_fakes.py
+
+Which is usually enough to trick an editor into importing the definitions without actually importing that into running code.
+
+We then get code completion on the __db__ object all the way down to table's field's attributes:
+
+        db.
+        db.my_table.
+        db.my_table.my_field.
+        db.my_table.my_field.represent
+
+The generated file also creates other global variables like "response" and "request" but just intsantiates them to an empty object.
+
+##Setup Instructions:
+
+1. Copy the file __auto_completion_support.py__ into the modules folder of a web2py application.
+2. Import the module and call __generate_auto_completion_file(db, file)__ from any place in your web2py app so long as the models have all been processed. (I call it from a function in a test controller in which I have imported the module)
+3. The file parameter is where you want the dummy classes to be written to (file contents will be overwritten).
+4. Fake import that file into any source file (controller, module) in which you want auto completion, like so:
+
+        if 0:
+            import autocomplete_fakes.py
+            from ide_code_completion import *
+            
+I add that second line to get code completion and hints on things like __SQLForm__ which is useful.
+
+You may need to adjust your IDE settings to include an extra python path to point to the file's location, and also include path to web2py source.
+
+##Getting help:
+
+This has only been tested on [PyScripter](https://code.google.com/p/pyscripter/) on Windows, should work for other IDEs, but that's up to you to figure out as each one is different.
+
+Feel free to improve and send me a pull request!
---
+++ auto_completion_support.py
@@ -0,0 +1,74 @@
+
+
+def generate_auto_completion_file(db, file_path):
+    """
+    This function generates a .py file with dummy class definitions mimicking
+    a number of fake global variables commonly used in web2py, so that you can get
+    auto-completion support in IDEs which support that (e.g. Pyscripter).
+    
+    For the "db" object it provides fakes all the way down to table's field attributes:
+
+        db.
+        db.my_table.
+        db.my_table.my_field.
+        db.my_table.my_field.represent
+
+    For other objects like "response" and "request" you get what you get.
+    To use get autocomplete in a controller or model, do a fake import:
+    
+    if 0:
+        import autocomplete_fakes.py # or whatever your file is called.
+    This is usually enough to trick the IDE.
+    
+    Be sure to call this function after all the tables have been defined on 'db' 
+    i.e. after all the models have executed.
+    """
+    
+    import os
+    f = open(file_path, 'w')
+    for line in [
+        '#!/usr/bin/python',
+        '#-*- coding: utf-8 -*-',
+        '#  This file defines a number of fake global variables commonly used in web2py,',
+        '#  simply to provide auto-completion support in an IDE.',
+        '#  For the "db" oject it provides fakes all the way down to table\'s field\'s attributes:',
+        '#  db.my_table.my_field.represent',
+        '#  For other objects like "response" and "request" you get what you get.',
+        '#  It is rebuilt from a running copy of db by the function generate_auto_completion_file()',
+        '#  which is most likely in the auto_completion_support.py file in modules.
+        '#',
+        'from gluon import *',
+        'from gluon.globals import *',
+        ]:
+        f.write('%s\n' % line)
+    f.write('\n\n')
+    tables = []
+    for table in db.tables:
+        tables.append(table)
+        f.write('\nclass %s:\n' % table.capitalize())
+        f.write('    def  __init__(self):\n')
+        for field in db[table].fields:
+            f.write('        self.%s = Field("dummy")\n' % field)
+        f.write('\n    def insert(self, %s):\n' % ', '.join(['%s=None' % field for field in db[table].fields]))
+        f.write('        pass\n\n')
+
+    f.write('\nclass FakeDAL(DAL):\n')
+    f.write('    def  __init__(self):\n')
+    for table in tables:
+        f.write('        self.%s = %s()\n' % (table,  table.capitalize()))
+    f.write('\n')
+    fake_globals = ['session', 'response', 'request', 'auth']
+    for i in fake_globals:
+        f.write('\nclass Fake%s(%s):\n' % (i.capitalize(), i.capitalize()))
+        f.write('    def  __init__(self):\n')
+        f.write('        pass\n')
+        f.write('%s = Fake%s()\n' % (i, i.capitalize()))
+    for line in [
+        'db = FakeDAL()',
+        "__all__ = ['db',%s]" % ''.join([" '%s'," % i for i in fake_globals]),
+        ]:
+        f.write('\n%s\n' % line)
+    f.close()
+
+
+
